43eb7a7711967c6b71d6326b56a4b02cc1ae017b
Wizorcerer <Elijah.hammer@outlook.com>
2025-09-24 19:30:45 -0600

    ponder

--- ponder/meta-css.js
+++ ponder/meta-css.js
@@ -1,37 +1,12 @@
-const PI = 3.14;
-let radius = 3;
-
-let area = radius * radius * PI;
-
-console.log(area);
-
-radius = 20;
-
-console.log(area)
-
-const one = 1;
-const two = '2';
-
-let result = one * two;
-console.log(result);
-
-result = one + Number(two);
-console.log(result);
-
-let course = "CSE131";
-if (true) {
-    let student = "John";
-    console.log(course);
-    console.log(student)
-}
-
-console.log(course)
-console.log(student)
-
-
 let selectElem = document.getElementById('webdevlist');
 selectElem.addEventListener('change', function(){
     let codeValue = selectElem.value;
     console.log(codeValue);
     document.getElementById(codeValue).style.color = 'red';
 })
+
+const image = document.querySelector('img');
+image.setAttribute('src', 'images/OIP.webp');
+image.setAttribute('alt', 'new logo');
+
+document.body.style.backgroundColor = 'lightblue';
