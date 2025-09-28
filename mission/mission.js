2f0ab358902f6158dcb76fb54ae0c281936ef7b9
Wizorcerer <Elijah.hammer@outlook.com>
2025-09-28 00:16:00 -0600

    c

--- mission/mission.js
+++ mission/mission.js
@@ -8,7 +8,7 @@ selectElem.addEventListener('change', changeTheme);
 function changeTheme() {
     let current = selectElem.value;
     if (current == 'dark') {
-        b.classList.add("darkmode");
+        document.body.classList.add("darkmode");
     } else {
         b.classList.remove("darkmode");
     }
