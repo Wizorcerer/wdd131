a2d05981c1238767b4e4f6b6b44c1c5f28f54a62
Wizorcerer <Elijah.hammer@outlook.com>
2025-09-27 23:41:38 -0600

    change

--- mission/mission.js
+++ mission/mission.js
@@ -7,10 +7,10 @@ selectElem.addEventListener('change', changeTheme);
 function changeTheme() {
     let current = selectElem.value;
     if (current == 'dark') {
-        document.body.style.background = '#333'
-        document.body.style.color = 'white'
-        logo.setAttribute("src", "https://wddbyui.github.io/wdd131/images/byui-logo-white.png")
+        document.body.style.background = "#333";
+        document.body.style.color = "white";
+        logo.setAttribute("src", "https://wddbyui.github.io/wdd131/images/byui-logo-white.png");
     } else {
-        document.body.style.background = 'white'
+        document.body.style.background = "white";
     }
 }
