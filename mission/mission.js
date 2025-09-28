e094c5f488374954e2637a078b339e52b267f9a4
Wizorcerer <Elijah.hammer@outlook.com>
2025-09-27 23:46:29 -0600

    c

--- mission/mission.js
+++ mission/mission.js
@@ -1,13 +1,14 @@
 
 let selectElem = document.querySelector('select');
 let logo = document.querySelector('img');
+let b = document.querySelector('body')
 
 selectElem.addEventListener('change', changeTheme);
 
 function changeTheme() {
     let current = selectElem.value;
     if (current == 'dark') {
-        document.body.style.backgroundColor = "#333";
+        b.style.backgroundColor = "#333";
         document.body.style.color = "white";
         logo.setAttribute("src", "https://wddbyui.github.io/wdd131/images/byui-logo-white.png");
     } else {
