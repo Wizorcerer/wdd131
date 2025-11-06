bf4cfbe57c0d00cafc1586ef12350d4ac0a11644
Wizorcerer <Elijah.hammer@outlook.com>
2025-11-05 17:39:57 -0700

    c

---
+++ charactercard/script.js
@@ -0,0 +1,43 @@
+const character = {
+    name: 'Gerald', 
+    class: 'Fighter',
+    maxHealth: 19, 
+    health: '19', 
+    level: 2, 
+}
+
+document.querySelector('#class').textContent = character.class;
+document.querySelector('#level').textContent = character.level;
+document.querySelector('#health').textContent = character.health;
+
+function levelUp() {
+    character.level += 1;
+    document.querySelector('#level').textContent = character.level;
+    character.maxHealth += 4;
+    character.health = character.maxHealth;
+    document.querySelector('#health').textContent = character.health;
+}
+
+const button = document.getElementById('leveling');
+button.addEventListener('click', levelUp);
+
+function attacked() {
+    character.health -= 3;
+    document.querySelector('#health').textContent = character.health;
+    if (character.health <= 0){
+    alert('You are dead!');
+    };
+}
+
+const button2 = document.getElementById('damaging');
+button2.addEventListener('click', attacked);
+
+function longRest() {
+    character.health = character.maxHealth;
+    document.querySelector('#health').textContent = character.health;
+}
+
+const button3 = document.getElementById('resting');
+button3.addEventListener('click', longRest);
+
+console.log(character.level);
